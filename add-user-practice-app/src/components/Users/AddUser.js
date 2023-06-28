f7a0aee3644d208f10fdf581a0d327274e47107a
sachin39 <sachin.kr.39@gmail.com>
2023-06-28 15:24:01 +0530

    used Refs in place of useState

--- add-user-practice-app/src/components/Users/AddUser.js
+++ add-user-practice-app/src/components/Users/AddUser.js
@@ -1,25 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
+  const nameInputRef = useRef(); //refs are used when you just want to read the value from a dom and you dont want to update dom. If you want to manipulate the dom you need to use states.
+  const ageInputRef = useRef(); //default value is null;
 
   const [error, setError] = useState();
   const addUserHandler = (event) => {
     event.preventDefault();
-
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    console.log(nameInputRef.current.value); //a json object will be output on the console and that object have a current property that holds the actual value of the element to which this ref connected to.
+    const eneteredName = nameInputRef.current.value;
+    const eneterdUserAge = ageInputRef.current.value;
+    if (
+      eneteredName.trim().length === 0 ||
+      eneterdUserAge.trim().length === 0
+    ) {
       setError({
         title: "Invalid input",
         message: "Please enter a valid name and age (non-empty values).",
       });
       return;
     }
-    if (+enteredAge < 1) {
+    if (+eneterdUserAge < 1) {
       //append + for comparing the string to a number because the enteredAge is a string bydefault.
       setError({
         title: "Invalid age",
@@ -29,22 +34,13 @@ const AddUser = (props) => {
     }
 
     const enteredUser = {
-      name: enteredUsername,
-      age: enteredAge,
+      name: eneteredName,
+      age: eneterdUserAge,
       id: Math.random().toString(),
     };
     props.onAddUser(enteredUser);
-    console.log(enteredUsername, enteredAge);
-    setEnteredUsername("");
-    setEnteredAge("");
-  };
-
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
-
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
   };
 
   const errorHandler = () => {
@@ -63,19 +59,9 @@ const AddUser = (props) => {
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username </label>
-          <input
-            id="username"
-            value={enteredUsername}
-            type="text"
-            onChange={usernameChangeHandler}
-          ></input>
+          <input id="username" type="text" ref={nameInputRef}></input>
           <label htmlFor="age">Age (Years) </label>
-          <input
-            id="age"
-            value={enteredAge}
-            type="number"
-            onChange={ageChangeHandler}
-          ></input>
+          <input id="age" type="number" ref={ageInputRef}></input>
           <Button type="submit">Add User</Button>
         </form>
       </Card>
